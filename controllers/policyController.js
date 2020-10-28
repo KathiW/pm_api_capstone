b8c6ada57b7d1bb90398295938d1894ca09ab327
Ward <KATHI.WARD@LibertyMutual.com>
2020-10-28 12:53:45 -0600

    update policy working

--- controllers/policyController.js
+++ controllers/policyController.js
@@ -15,3 +15,52 @@ exports.policy_create_post = function(req,res) {
     });
 
     };
+
+    exports.policy_list_get = function(req,res){
+        Policy.find()
+        .then(data => {
+            res.status(200).json(data);
+        })
+        .catch(err => {
+            res.status(400).send('Error when retrieving policies');
+        });
+
+    }
+
+    exports.getById = (req,res) => {
+        const id = req.params.id;
+        console.log(`>>>GET the policy information for ${id}...`);
+        if (id != null && id != ''){
+            Policy
+            .findById(id)
+            .then(data => {
+                if(!data){
+                    res.status(404).send({Message: `No Policy Data found with ID: ${id}.`});
+                }
+                else {
+                    res.status(200).json(data);
+                }            
+        })
+        .catch(error => {res.status(400).send({ErrorMessage: error.message});
+        });
+    }
+    };
+
+    exports.updateById = (req,res) => {
+        const id = req.params.id;
+       // console.log(`>>>GET the policy information for ${id}...`);
+        if (id != null && id != ''){
+            Policy
+            .findOneAndUpdate(id, req.body, {new:true})
+            .then(data => {
+                if(!data){
+                    res.status(404).send({Message: `No Policy Data found with ID: ${id}.`});
+                }
+                else {
+                    res.status(200).json(data);
+                }            
+        })
+        .catch(error => {res.status(400).send({ErrorMessage: error.message});
+        });
+    }
+    };
--- routes/policyRouter.js
+++ routes/policyRouter.js
@@ -3,11 +3,18 @@ var router = express.Router();
 var policy_controller = require('../controllers/policyController');
 
 //Policy page route
-router.get('/', function (req, res){
-    res.send('Policy home page');
-})
+router.get('/',policy_controller.policy_list_get);
+
+// router.get('/', function (req, res){
+//     res.send('Policy home page');
+// })
 
 
 router.post('/', policy_controller.policy_create_post);
+
+//Get policy by ID
+router.get("/:id", policy_controller.getById);
+
+router.put("/:id", policy_controller.updateById);
 
 module.exports = router;
